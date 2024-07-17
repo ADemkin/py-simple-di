fa99f54b86e693e12c4e07f7f7b5aa003cc922e6
Anton Demkin <avdemkin@avito.ru>
2024-07-17 12:08:32 +0400

    Add circular dependency protection

--- magic_provider.py
+++ magic_provider.py
@@ -1,8 +1,6 @@
-import asyncio
 from dataclasses import dataclass
 from dataclasses import field
-from functools import partial
-from functools import wraps
+from contextlib import contextmanager
 from typing import Any
 from typing import Mapping
 from typing import Callable
@@ -12,6 +10,7 @@ from typing import ParamSpec
 from typing import Type
 from typing import TypeVar
 from typing import Self
+from typing import Generator
 from typing import get_type_hints
 
 
@@ -32,6 +31,7 @@ def name(obj: Any) -> str:
 @dataclass(slots=True, frozen=True)
 class Provider(Generic[T]):
     _instances: dict[str, T] = field(default_factory=dict)
+    _stack: set[str] = field(default_factory=set)
 
     @classmethod
     def create(
@@ -46,15 +46,35 @@ class Provider(Generic[T]):
     def register_instance(self, instance: T) -> None:
         self._instances[name(instance)] = instance
 
-    def gather_dependencies(self, obj: Type[T] | Callable[P, Any]) -> Mapping[str, T]:
+    @contextmanager
+    def _circular_dependency_protection(
+            self,
+            obj: Type[T] | Callable[P, T],
+            dep: Type[T],
+    ) -> Generator[None, None, None]:
+        dep_name = name(dep)
+        if dep_name in self._stack:
+            message = f"circular dependency between {dep_name!r} & {name(obj)!r}"
+            raise DependencyInjectionError(message)
+        self._stack.add(dep_name)
+        yield
+        self._stack.discard(dep_name)
+
+    def gather_dependencies(
+        self, obj: Type[T] | Callable[P, T]
+    ) -> Mapping[str, T]:
         try:
             type_hints = get_type_hints(obj)
         except NameError as err:
             message = f"unable to resolve dependency: {err.name!r}"
             raise DependencyInjectionError(message) from err
-        return {arg: self.build(dep) for arg, dep in type_hints.items()}
+        kwargs = {}
+        for arg, dep in type_hints.items():
+            with self._circular_dependency_protection(obj, dep):
+                kwargs[arg] = self.build(dep)
+        return kwargs
 
-    def build(self, cls: Type[T], *args, **kwargs) -> T:
+    def build(self, cls: Type[T], *args: P.args, **kwargs: P.kwargs) -> T:
         if instance := self._instances.get(name(cls)):
             return instance
         dependencies = self.gather_dependencies(cls)
@@ -100,7 +120,7 @@ if __name__ == "__main__":
     @dataclass
     class Missing:
         logger: Logger
-        converter: 'Converter'
+        converter: "Converter"
 
     try:
         provider.build(Missing)
@@ -109,3 +129,21 @@ if __name__ == "__main__":
     else:
         raise AssertionError("expected DependencyInjectionError")
     print("missing dependency test passed")
+
+    # test circular depedency
+    class A:
+        b: 'B'
+
+    class B:
+        a: A
+
+    provider = Provider()
+    try:
+        provider.build(A)
+    except DependencyInjectionError as err:
+        assert "circular dependency between" in str(err)
+        assert "'A'" in str(err)
+        assert "'B'" in str(err)
+    else:
+        raise AssertionError("expected DependencyInjectionError")
+    print("circular dependency test passed")
